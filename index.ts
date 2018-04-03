e8395e76fd52e9625156e05673dd73b2dfb7765a
mgechev <mgechev@gmail.com>
2018-04-02 19:04:46 -0700

    refactor: improve style

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
 const opn = require('opn');
-import { post } from 'request';
+import { post } from 'request-promise';
 import { parse } from 'url';
 import * as express from 'express';
+import { Server } from 'http';
 
-const RedirectURI = 'http://localhost:7361';
 const OAuth2TokenURL = 'https://www.googleapis.com/oauth2/v4/token';
+const OAuth2CodeURL = 'https://accounts.google.com/o/oauth2/v2/auth';
 
 export interface Config {
   clientId: string;
@@ -13,72 +14,66 @@ export interface Config {
 }
 
 export const auth = (config: Config) => {
-  const OAuth2CodeURL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${
-    config.clientId
-  }&redirect_uri=${RedirectURI}&response_type=code&scope=${config.scope}&code_challenge_method=plain`;
-
-  opn(OAuth2CodeURL).then((cp: any) => cp.kill());
-
-  return new Promise(listen);
+  return listenForTokens((port: number) =>
+    opn(
+      `${OAuth2CodeURL}?client_id=${config.clientId}&redirect_uri=http://localhost:${port}&response_type=code&scope=${
+        config.scope
+      }`,
+      { wait: false }
+    )
+  );
 };
 
-const listen = (resolve: Function, reject: Function) => {
+const listenForTokens = (ready: Function) => {
   const app = express();
+  const socketList: any[] = [];
+
+  let server: Server;
+  let port: number;
+
+  let resolveCallback: Function;
+  let rejectCallback: Function;
+
+  const cleanup = (cb: Function, res: express.Response, result: any) => {
+    res.end();
+    socketList.forEach(s => s.destroy());
+    server.close(() => cb(result));
+  };
 
   app.get('/', (req, res) => {
+    const resolve = cleanup.bind(null, resolveCallback, res);
+    const reject = cleanup.bind(null, rejectCallback, res);
+
     const params = parse(req.url, true);
     const { code, error } = params.query;
+
     if (error) {
       reject(error);
-      server.close();
     } else {
-      post(
-        OAuth2TokenURL,
-        {
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-          },
-          body: encodeURI(
-            `client_id=${ClientID}&client_secret=${ClientSecret}&redirect_uri=${RedirectURI}&grant_type=authorization_code&code=${code}`
-          )
+      post(OAuth2TokenURL, {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
         },
-        (error, response) => {
-          if (error) {
-            reject(error);
-            server.close();
-          } else {
-            let body: null | string = null;
-            try {
-              body = JSON.parse(response.body);
-            } catch (e) {
-              reject(e);
-              server.close();
-              return;
-            }
-            resolve(body);
-            server.close();
-          }
-        }
-      );
+        body: encodeURI(
+          `client_id=${ClientID}&client_secret=${ClientSecret}&redirect_uri=http://localhost:${port}&grant_type=authorization_code&code=${code}`
+        )
+      }).then(response => resolve(JSON.parse(response)), reject);
     }
-    res.end();
   });
-  const server = app.listen(7361);
+
+  server = app.listen(0, () => {
+    port = server.address().port;
+    ready(port);
+
+    // Used for speeding up `server.close()`.
+    server.on('connection', socket => {
+      socketList.push(socket);
+      socket.on('close', () => socketList.splice(socketList.indexOf(socket, 1)));
+    });
+  });
+
+  return new Promise((s, f) => {
+    resolveCallback = s;
+    rejectCallback = f;
+  });
 };
-
-const ClientID = '329457372673-hda3mp2vghisfobn213jpj8ck1uohi2d.apps.googleusercontent.com';
-const ClientSecret = '4camaoQPOz9edR-Oz19vg-lN';
-const Scope = 'https://www.googleapis.com/auth/analytics.readonly';
-
-auth({
-  clientId: ClientID,
-  clientSecret: ClientSecret,
-  scope: Scope
-}).then(
-  res => {
-    console.log(res);
-  },
-  err => {
-    console.error(err);
-  }
-);
